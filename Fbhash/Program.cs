4558beb7e9769a8059daee45db28574f4471bcc3
Patrick H Daniel Jr <patrickhdaniel@outlook.com>
2020-06-04 15:41:03 -0500

    Add project files.

---
+++ Fbhash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fbhash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
